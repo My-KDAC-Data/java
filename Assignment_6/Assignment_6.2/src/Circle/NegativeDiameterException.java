c0986a3198aad1ae8f80e2e6ae24985ae88ae04d
sunbeam <shubhamwalke.edu@gmail.com>
2023-10-18 15:07:13 +0530

    Assignment 6 uploaded

---
+++ Assignment_6/Assignment_6.2/src/Circle/NegativeDiameterException.java
@@ -0,0 +1,21 @@
+package Circle;
+
+public class NegativeDiameterException extends RuntimeException
+{
+//	String message;
+	
+	public NegativeDiameterException() 
+	{
+	}
+	public NegativeDiameterException(String message) 
+	{
+		super(message);
+	}
+
+	@Override
+	public String toString() {
+		return "NegativeDiameterException : " + getMessage() ;
+	}
+	  
+
+}
